f4194d3990decc2f114c126c03c45dfc2642aca7
JosueDev-afk <josue_ceballos12@hotmail.com>
2023-05-28 02:02:02 -0600

    paginaPrincipal

--- portal/src/components/formulario/formulario.js
+++ portal/src/components/formulario/formulario.js
@@ -1,13 +1,11 @@
 import React from 'react';
-
 class Formulario extends React.Component {
     render() {
-      return (
+        return(
         <div>
-          <h1>Hola, soy un componente de React</h1>
-        </div>
+        <h1>Hola, soy un componente de formulario</h1>
+      </div>
       );
-    }
   }
-  
+}
   export default Formulario;
--- portal/src/components/tarjetaCredito/tarjetaCredito.js
+++ portal/src/components/tarjetaCredito/tarjetaCredito.js
@@ -1,11 +1,29 @@
-class tarjetaCredito extends React.Component {
-    render() {
+import React, { useState } from "react";
+import { Button } from 'react-bootstrap';
+import App from "../../App";
+function TarjetaCredito() {
+    const [mostrarTarjeta, setTarjeta] = useState(false);
+
+    const handleClickTarjeta=()=>{
+        setTarjeta(!mostrarTarjeta);
+      };
+
       return (
         <div>
-          <h1>Hola, soy un componente de React</h1>
+        
+            <React.Fragment>
+            
+              {!mostrarTarjeta && 
+                <>
+                
+                <h1>Banorte</h1>
+                <Button variant="primary" onClick={handleClickTarjeta}>Regresar</Button></>
+              }
+              {mostrarTarjeta && <App />}
+            
+            </React.Fragment>
         </div>
       );
-    }
   }
   
-  export default tarjetaCredito;
+  export default TarjetaCredito;
