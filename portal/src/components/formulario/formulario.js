a46360d4731d33bc1a9ccceb57b5213c840b2729
JosueDev-afk <josue_ceballos12@hotmail.com>
2023-05-27 19:07:40 -0600

    componentes

---
+++ portal/src/components/formulario/formulario.js
@@ -0,0 +1,11 @@
+class formulario extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hola, soy un componente de React</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default formulario;
---
+++ portal/src/components/tarjetaCredito/tarjetaCredito.js
@@ -0,0 +1,11 @@
+class tarjetaCredito extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hola, soy un componente de React</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default tarjetaCredito;
